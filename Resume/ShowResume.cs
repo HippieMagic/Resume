318eb5c21959f8781ebed9136f4630a72d57c276
Nicholas Moore <nick_r_moore@hotmail.com>
2021-04-26 04:30:29 -0400

    Commit with basic functionality. So I don't lose it.

--- Resume/ShowResume.cs
+++ Resume/ShowResume.cs
@@ -1,10 +1,32 @@
 ﻿using System;
 namespace Resume
 {
+    // Just using this class to help make adding the resume in easier later and to show I can actually make a class... sometimes.
     public class ShowResume
     {
-        public ShowResume()
+
+        public void Introduction()
         {
+            Console.WriteLine("Former .NET developer with experience in modern web technologies and mobile application development.");
+            Console.WriteLine("Looking to re-enter the field as a junior or mid-level developer.");
         }
+
+        public void Education()
+        {
+            Console.WriteLine("Regent's Bachelor of Arts - WVU - 2021.");
+        }
+
+        public void Experience()
+        {
+            Console.WriteLine("Prosite Business Solutions 2014-2018");
+            Console.WriteLine("Full Stack ASP.NET Developer.");
+            Console.WriteLine("HTML, CSS, JavaScript, SQL, Visual Basic.");
+        }
+
+        public void Contact()
+        {
+            Console.WriteLine("nick_r_moore@hotmail.com");
+        }
+
     }
 }
