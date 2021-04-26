c77b2ad6816dc7c1961461ef24d72f1ffb4937e2
Nicholas Moore <nick_r_moore@hotmail.com>
2021-04-26 11:23:14 -0400

    Going to use this class to build the menu dynamically and clean the main project up.

---
+++ Resume/MenuBuilder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Resume
+{
+    public class MenuBuilder
+    {
+        // going to use this class to build the menu dynamically.
+        public MenuBuilder()
+        {
+        }
+    }
+}
